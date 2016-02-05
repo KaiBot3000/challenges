d5f5637bb8098ebf748e390a39a370857718b846
Kai <riselikethemoon@gmail.com>
2016-02-05 14:44:09 -0800

    totalDistance and shortestDetour functional, off by one?

--- cars.py
+++ cars.py
@@ -1,3 +1,5 @@
+import math
+
 def shortestDetour(points):
     """Calculate the detour distance between two different rides. 
 
@@ -8,6 +10,42 @@ def shortestDetour(points):
     """
 
     trip_a_start, trip_a_stop, trip_b_start, trip_b_stop = points
-    print trip_a_start, trip_a_stop, trip_b_start, trip_b_stop
+    # print trip_a_start, trip_a_stop, trip_b_start, trip_b_stop
+    a_trip = [trip_a_start, trip_a_stop]
+    print "atrip", a_trip
+    a_drops = [trip_a_start, trip_b_start, trip_b_stop, trip_a_stop]
+    print "adrops", a_drops
+    a_detour = totalDistance(a_drops) - totalDistance(a_trip)
+    print "adetour", a_detour
 
-shortestDetour([(3, 5), (2, 4), (5, 8), (7, 9)])
+    b_trip = [trip_b_start, trip_b_stop]
+    b_drops = [trip_b_start, trip_a_start, trip_a_stop, trip_b_stop]
+    b_detour = totalDistance(b_drops) - totalDistance(b_trip)
+    print "btrip", b_trip
+    print "bdrop", b_drops
+    print "bdetour", b_detour
+
+    
+    shortest = min(a_detour, b_detour)
+
+    return shortest
+
+
+
+def totalDistance(route):
+    """Given a list of point tuples, determine the total distance travelled"""
+
+    distance = 0
+    start = None
+
+    for point in route:
+        if start:
+            distx = abs((start[0] - point[0]))
+            disty = abs((start[1] - point[1]))
+            distance += math.sqrt((distx**2) + (disty**2))
+        start = point
+
+    return distance
+
+# print shortestDetour([(1,1), (1,4), (1,2), (1,3)])
+print totalDistance([(0,0), (3, 4)])
